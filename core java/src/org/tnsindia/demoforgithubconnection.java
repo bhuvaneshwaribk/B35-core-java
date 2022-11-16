75399d841f7f7fdfe0cd25e4bc93094e750f328f
bhuvaneshwari <bhuvaneshwaribk2000@gmail>
2022-11-16 21:33:01 +0530

    first commit

---
+++ core java/src/org/tnsindia/demoforgithubconnection.java
@@ -0,0 +1,11 @@
+package org.tnsindia;
+
+public class demoforgithubconnection {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to TNS india foundation");
+
+	}
+
+}

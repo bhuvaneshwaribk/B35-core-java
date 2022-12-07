3bc16d02e80c1f137284de9af20f1ebfccb6ef74
bhuvaneshwari <bhuvaneshwaribk2000@gmail>
2022-12-07 20:20:12 +0530

    first commit

---
+++ Day12_String_StringBuffer_StringBuilder/src/stringBuilderDemo/StringBuliderExample.java
@@ -0,0 +1,18 @@
+package stringBuilderDemo;
+
+public class StringBuliderExample {
+
+	public static void main(String[] args) {
+		StringBuilder s=new StringBuilder("helo");
+		//capacity method
+		System.out.println(s.capacity());
+		System.out.println(s.length());
+		System.out.println(s.append("bhuvi"));
+		System.out.println(s.insert(4, " "));
+
+
+
+
+	}
+
+}
